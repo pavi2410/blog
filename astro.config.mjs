45f5f5dfc56293f83e94f340ab08c72c02c2d1e2
Pavitra Golchha <hello@pavi2410.me>
2023-04-17 00:36:30 +0530

    Bump deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,8 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://blog.pavi2410.me',
-  integrations: [preact(), tailwind()],
-  markdown: {
-    shikiConfig: {
-      theme: 'one-dark-pro',
-    }
-  }
+  integrations: [tailwind()],
 });
